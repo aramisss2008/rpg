91bddd5bd971cfe1eba8df0eaf03e728d6ecbdfe
aramisss2008 <96666132+aramisss2008@users.noreply.github.com>
2021-12-26 13:40:57 +0300

    del

--- pythonProject2/test2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def xxx(x,y,z):
-    print(x,y,z)
-
-a = np.matrix([0.001,0.2,3]).tolist()[0]
-
-xxx( *a)
-
--- pythonProject2/test3.py
+++
@@ -1,6 +0,0 @@
-list1 = [7, 5, 3, 1, 3, 22, 23, -2, 3]
-sum = 0
-for i in range(len(list1)):
-    if list1[i] % 2 == 0:
-        sum += list1[i]
-print(sum)
