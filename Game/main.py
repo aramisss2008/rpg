5f8ac32ced628c832954ddb67ca8cfc41e235159
aramisss2008 <96666132+aramisss2008@users.noreply.github.com>
2022-01-09 15:03:27 +0300

    add create

--- Game/main.py
+++ Game/main.py
@@ -7,13 +7,35 @@ print("create hero-2")
 code = int(input())
 if code ==1:
     print("write name")
-    chooseName = input()
+    chooseName = input() #TODO: предлогать персонажей
     load(chooseName + ".txt")
 if code ==2:
-    name = "hero"
-    class_h = "lancer"
+    print(" enter name")
+    name = input()
+
+    print("chose class: 1. lancer 2. wizard 3. archer, 4. summoner")
+    dict = {"1": "lancer", "2": "wizard", "3": "archer", "4": "summoner"}
+    num = input()
+    while 1:
+        if dict.get(num) == None:
+            print("chose rigth number!")
+            num = input()
+        else:
+            break
+    class_h = dict.get(num)
+
+    print("chose race: 1.orc , 2.human, 3.dwarf")
+    dict1 = {"1" : "orc", "2" : "human", "3" : "dwarf"}
+    num1 = input()
+    while 1:
+        if dict1.get(num1) == None:
+            print("chose rigth number!")
+            num1 = input()
+        else:
+            break
+    racem = dict1.get(num1)
+
     goldm = 0
-    racem = "human"
     lvl = 1
     myHero = hero(name,class_h,goldm,racem,lvl)
     myHero.save()
---
+++ Game/test.py
@@ -0,0 +1,2 @@
+dict = {"1" : "a", 2: "q", 3: "we"}
+print( dict["1k"] )
